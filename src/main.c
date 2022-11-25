c9433e3431e950245b7611130ab0a2cf2ec3275d
Nathan Chell <zotecfury@gmail.com>
2022-11-25 12:05:45 +0000

    Made work on file handeling

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ struct cords
 {
 	int x;
 	int y;
-}
+};
 
 /*void display(struct cords c);
 
@@ -22,13 +22,13 @@ void display(struct cords c, char* arr){
 
 */
 
-char read_file(char* path, char* arr)
-{
+char read_file(char* path, char* arr){
 	
-	char file_contents[100];
-	char * pch;
-	int i;
+	char file_contents[10000][10];
+	char line[2]; /* Line is of size 2, thus the maximum size of the total board is 99 x 99*/
 	FILE *filePointer; 
+	char temp_str[10];
+	int counter = 0, i, j;
 
 	filePointer = fopen(path, "r");
 	
@@ -38,22 +38,32 @@ char read_file(char* path, char* arr)
 		exit(1);
 	}
 	
-	fscanf(filePointer, "%s", &file_contents);
-	fclose(filePointer);
-	
-	for(i; i < sizeof(file_contents); i++){
-		pch = strtok (file_contents, " ");
-		while(pch != NULL)
-		{
-			printf("%s\n", pch)	;
+	while(fscanf(filePointer, "%s", &line) != EOF){
+		if (i < 3){
+			strcat(temp_str, line);
+			i++;
+		}else{
+			printf("%s", temp_str);
+			i = 0;
+			counter++;
+			/*file_contents[counter] = temp_str;*/
+			strncpy(temp_str, file_contents[counter], 10);
+			temp_str[0] = "\0";
 		}
-	}	
+	}
+	
+	/*
+	for (i=0; i < (sizeof(file_contents) / sizeof(file_contents[0])); i++){
+		printf("%c", &file_contents[i]);
+	};
+	*/
+	
 }
 
 int main(int argc, char *argv[]) {
 	
 	char Board[20][40];
-	char path[] = "./maze.txt";
+	char path[] = "../Data/maze.txt";
 	
 	read_file(path, Board);
 	return 0;
